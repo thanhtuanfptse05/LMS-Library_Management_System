cb0bc609f519c2fabf00a9ddd42918613879fbed
cchuong1123 <cchuong1123@gmail.com>
2026-06-10 18:42:30 +0700

    feat(category): extend category model

--- src/java/model/Category.java
+++ src/java/model/Category.java
@@ -1,10 +1,17 @@
 package model;
 
+import java.sql.Timestamp;
+
 public class Category {
 
     private int categoryId;
     private String name;
     private String description;
+    private String status;
+    private Timestamp updatedAt;
+    private Integer updatedBy;
+    private String updatedByName;
+    private int bookCount;
 
     public int getCategoryId() {
         return categoryId;
@@ -28,5 +35,45 @@ public class Category {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Integer updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public String getUpdatedByName() {
+        return updatedByName;
+    }
+
+    public void setUpdatedByName(String updatedByName) {
+        this.updatedByName = updatedByName;
+    }
+
+    public int getBookCount() {
+        return bookCount;
+    }
+
+    public void setBookCount(int bookCount) {
+        this.bookCount = bookCount;
     }
 }
