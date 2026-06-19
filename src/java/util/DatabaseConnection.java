80635e2ac40359e1c317e9ac1e0d75b965b72aab
MeHocCode <lethebaoonepiece@gmail.com>
2026-06-19 11:45:20 +0700

    fix(db): switch connection port to 5432 to use Session Pooler over IPv4

--- src/java/util/DatabaseConnection.java
+++ src/java/util/DatabaseConnection.java
@@ -21,7 +21,7 @@ public class DatabaseConnection {
     private static final Logger LOGGER = Logger.getLogger(DatabaseConnection.class.getName());
 
     private static final String DRIVER = "org.postgresql.Driver";
-    private static final String URL = "jdbc:postgresql://aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require&prepareThreshold=0&preparedStatementCacheQueries=0";
+    private static final String URL = "jdbc:postgresql://aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
     private static final String USER = "postgres.wukwrfwdrbstyoqissjz";
     private static final String PASSWORD = "6wUw)Q6S/)LFeSE";
 
