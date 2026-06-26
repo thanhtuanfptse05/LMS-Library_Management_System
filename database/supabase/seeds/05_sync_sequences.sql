bae5d06a196fe1b0fd7d39ce60ad1f0b32f42bf3
MeHocCode <lethebaoonepiece@gmail.com>
2026-06-26 18:29:22 +0700

    fix(seed): update 05_sync_sequences to sync EmailTemplate instead of DocumentTemp

--- database/supabase/seeds/05_sync_sequences.sql
+++ database/supabase/seeds/05_sync_sequences.sql
@@ -8,4 +8,4 @@
 SELECT setval(pg_get_serial_sequence('"User"', 'userid'), COALESCE(MAX(userId), 1)) FROM "User";
 SELECT setval(pg_get_serial_sequence('Category', 'categoryid'), COALESCE(MAX(categoryId), 1)) FROM Category;
 SELECT setval(pg_get_serial_sequence('Tag', 'tagid'), COALESCE(MAX(tagId), 1)) FROM Tag;
-SELECT setval(pg_get_serial_sequence('DocumentTemp', 'tempid'), COALESCE(MAX(tempId), 1)) FROM DocumentTemp;
+SELECT setval(pg_get_serial_sequence('EmailTemplate', 'templateid'), COALESCE(MAX(templateId), 1)) FROM EmailTemplate;
